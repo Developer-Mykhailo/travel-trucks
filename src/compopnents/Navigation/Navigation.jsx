44f06e60e9782a4f12b3f043dc3474bb46fe6646
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-07-15 02:47:21 -0600

    make Home Page

--- src/compopnents/Navigation/Navigation.jsx
+++ src/compopnents/Navigation/Navigation.jsx
@@ -3,34 +3,41 @@ import s from "./Navigation.module.css";
 import logo from "../../assets/logo.svg";
 
 import { Link, NavLink } from "react-router-dom";
+import Container from "../Container/Container";
 
 const Navigation = () => {
   return (
     <>
       <header className={s.header}>
-        <nav className={s.nav}>
-          <Link to="/">
-            <img src={logo} alt="Travel Trucks logo" />
-          </Link>
-          <ul>
-            <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) => clsx(s.link, isActive && s.accent)}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/catalog"
-                className={({ isActive }) => clsx(s.link, isActive && s.accent)}
-              >
-                Catalog
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+        <Container>
+          <nav className={s.nav}>
+            <Link to="/">
+              <img src={logo} alt="Travel Trucks logo" />
+            </Link>
+            <ul>
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    clsx(s.link, isActive && s.accent)
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/catalog"
+                  className={({ isActive }) =>
+                    clsx(s.link, isActive && s.accent)
+                  }
+                >
+                  Catalog
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </Container>
       </header>
     </>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,29 @@
-// import React from "react";
+import { Link } from "react-router-dom";
+import Container from "../../compopnents/Container/Container";
+import s from "./HomePage.module.css";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <section className={s.hero}>
+      <Container>
+        <div className={s.content}>
+          <h1 className={s.title} tabIndex={0}>
+            Campers of your dreams
+          </h1>
+          <p className={s.descr}>
+            You can find everything you want in our catalog
+          </p>
+          <Link
+            className={s.view_now}
+            to="/catalog"
+            aria-label="link to catalog"
+          >
+            View Now
+          </Link>
+        </div>
+      </Container>
+    </section>
+  );
 };
 
 export default HomePage;
